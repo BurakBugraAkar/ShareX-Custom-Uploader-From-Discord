917e6c81061c0446b3356fa8bc2666f3ed66cafc
BurakBugraAkar <74423754+BurakBugraAkar@users.noreply.github.com>
2021-08-22 13:48:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 export default
 {
-	"port": "80",
+    "port": "80",
     "api_key": "ryukakar",
-    "discord_bot_token": "NjUwMDUwMzUwOTEyNjM0ODg0.XeFskQ.jAQ-gR3UoQmnV5cX_fvwBmCTWkY",
+    "discord_bot_token": "token",
     "guild": {
-        "id": "730823956545929246",
+        "id": "guild_id",
         "webhook": {
-            "wehbook_id": "878742194851303444",
-            "webhook_token": "aGBhceSQjtJHkGj25ecPvKApULOnsyzrv4omKIIQdZ5232tEr6ZkmVUZFzm4xlKemQL9"
+            "wehbook_id": "id",
+            "webhook_token": "token"
         },
         "channel": {
-            "image_log": "878759517314220123",
-            "ip_log": "878759552860971041"
+            "image_log": "channel_id",
+            "ip_log": "channel_id"
         }
     }
 }
