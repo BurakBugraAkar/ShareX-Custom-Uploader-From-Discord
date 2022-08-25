0cb3410f653925d24515100c14bd66fd39801dd2
BurakBugraAkar <burakbugraakar@gmail.com>
2022-08-25 12:01:06 +0300

    chore: file has been edited

--- api.mjs
+++ api.mjs
@@ -7,28 +7,21 @@ const app = express();
 import sh from 'shortid';
 import config from "./config.js"
 import { JsonDatabase } from "wio.db";
-
 const db = new JsonDatabase({ databasePath:"./images.json" });
 
 app.use(express.static('public'));
 app.listen(config.port)
 
-
 const hook = new Discord.WebhookClient(config.guild.webhook.wehbook_id, config.guild.webhook.webhook_token);
 app.use(fileUpload());
 app.post('/upload', function (req, res) {
-    if(req.header("api_key")!==config.api_key){
-        return res.sendStatus(403);
-    }
-    if (!req.files)
-        return res.status(400).send('No files were uploaded.');
+    if(req.header("api_key")!==config.api_key) return res.sendStatus(403);
+    if (!req.files) return res.status(400).send('No files were uploaded.');
     let sampleFile = req.files.sampleFile;
     let filename = sh.generate();
     const atac = new Discord.MessageAttachment(sampleFile.data)
     hook.send(atac).then(a => {
-        console.log(filename)
         db.set(filename, a.attachments[0].url)
-        //console.log(req)
         res.send(`http://${req.headers.host}/images/${filename}`)
         let embed = new Discord.MessageEmbed()
             .setColor("2f3136")
@@ -38,19 +31,19 @@ app.post('/upload', function (req, res) {
     })
 });
 
-
 app.get('/images/:filename', function(req, res) {
     let search = db.fetch(req.params.filename)
-    console.log(search)
     if (search) {
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         ip = ip.replace("::ffff:", "")
-        const embed = new Discord.MessageEmbed()
+        if (config.guild.channel.ip_log) {
+            const embed = new Discord.MessageEmbed()
             .setColor("2f3136")
             .setTitle("İp Logger")
             .setDescription(`User ip: ${ip}\nContent viewed: ${req.params.filename}`)
             .setTimestamp()
-        client.channels.cache.get(config.guild.channel.ip_log).send(embed)
+            client.channels.cache.get(config.guild.channel.ip_log).send(embed)
+        }
         request({
                 url: search,
                 encoding: null
@@ -65,8 +58,6 @@ app.get('/images/:filename', function(req, res) {
         return res.sendStatus(404);
     }
 });
-
-
 
 client.on('ready', () => {
     console.log(`Discord api bağlantısı ${client.user.tag} adı ile başarıyla yapıldı!`);
